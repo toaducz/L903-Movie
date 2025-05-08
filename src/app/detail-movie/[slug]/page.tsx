ddee67d5779e5bd645e4c81e5a940ed98ced81d4
Master <ducdeptraivodich23@gmail.com>
2025-05-08 18:04:57 +0700

    update smooth scrolling

--- src/app/detail-movie/[slug]/page.tsx
+++ src/app/detail-movie/[slug]/page.tsx
@@ -84,6 +84,7 @@ export default function WatchPage() {
                   setIsWatching(true)
                   const firstEp = data?.episodes?.[0]?.server_data?.[0]
                   if (firstEp) handleSelectEpisode(firstEp.link_m3u8)
+                  window.scrollTo({ top: 0, behavior: 'smooth' })
                 }}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -233,13 +234,13 @@ export default function WatchPage() {
           ) : (
             <div className="flex items-center justify-center h-96 text-xl">
               {!isAvailable ? (
-              <div className="flex items-center">
-                <svg className="animate-spin -ml-1 mr-3 h-8 w-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-                Đang tải tập phim...
-              </div>) : (<div>Phim chưa cập nhật...</div>)}
+                <div className="flex items-center">
+                  <svg className="animate-spin -ml-1 mr-3 h-8 w-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                  Đang tải tập phim...
+                </div>) : (<div>Phim chưa cập nhật...</div>)}
             </div>
           )}
         </div>
