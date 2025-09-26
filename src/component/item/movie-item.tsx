94fee45f39c274837babc1c4a8d87563ae6df772
phan duc <phanhuynhtoanduc@gmail.com>
2025-09-26 11:16:44 +0700

    add place holder movie item

--- src/component/item/movie-item.tsx
+++ src/component/item/movie-item.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { Movie } from '@/api/kkphim/get-update-movie'
 import React, { useState } from 'react'
 import ouguricap from '@/assets/xin-loi-ouguri-cap-cua-toi-an-het-anh-roi.jpg'
+import ouguricapLoading from '@/assets/xin-loi-ouguri-cap-cua-toi-an-het-anh-roi.png'
 
 type Props = {
   movie: Movie
@@ -16,11 +17,11 @@ export default function MovieItem({ movie, color, source }: Readonly<Props>) {
   const [isLoaded, setIsLoaded] = useState(false)
 
   const normalizePosterUrl = (posterUrl: string | object | null | undefined) => {
-    if (!posterUrl) return ouguricap // null, undefined, empty
-    if (typeof posterUrl === 'object') return ouguricap // object trả về ouguricap
+    if (!posterUrl) return ouguricapLoading // null, undefined, empty
+    if (typeof posterUrl === 'object') return ouguricapLoading // object trả về ouguricap
 
     const temp = String(posterUrl).trim()
-    if (temp === '{}' || temp === '') return ouguricap // object stringify hoặc empty string
+    if (temp === '{}' || temp === '') return ouguricapLoading // object stringify hoặc empty string
 
     if (!temp.startsWith('http')) {
       return `https://phimimg.com/${temp.replace(/^\/+/, '')}`
@@ -35,16 +36,22 @@ export default function MovieItem({ movie, color, source }: Readonly<Props>) {
       href={source ? `/${source}/detail-movie/${movie.slug}` : `/detail-movie/${movie.slug}`}
       className={`block ${color ?? 'bg-slate-800'} rounded-xl shadow-md hover:shadow-lg transition duration-300 overflow-hidden hover:opacity-80 cursor-pointer`}
     >
-      <Image
-        unoptimized
-        priority
-        src={poster ?? ''}
-        alt={movie.name}
-        width={270}
-        height={300}
-        onLoad={() => setIsLoaded(false)}
-        className={`w-full object-cover h-[270px] ${isLoaded ? 'opacity-0' : 'opacity-100'}`}
-      />
+      <div className="relative w-[auto] h-[300px]">
+        {!isLoaded && (
+          <div className="absolute inset-0 bg-gray-700 animate-pulse rounded-lg" />
+        )}
+        <Image
+          unoptimized
+          priority
+          src={poster ?? ouguricap}
+          alt={movie.name}
+          width={270}
+          height={300}
+          onLoad={() => setIsLoaded(true)}
+          className={`w-full h-full object-cover transition-opacity duration-500 ${isLoaded ? 'opacity-100' : 'opacity-0'
+            }`}
+        />
+      </div>
 
       <div className='p-4'>
         <h2 className='text-md font-semibold text-white h-13 line-clamp-2 overflow-hidden'>{movie.name}</h2>
